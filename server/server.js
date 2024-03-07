ca436e7fada09f371e53d09ea5c137993feef526
unknown <haythem.nasfi@gmail.com>
2024-03-07 12:46:17 +0100

    Mon-Espace

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const auth = require('./middelware/auth');
 
 connection();
 const app = express();
-const port = 5000;
+const port = 4000;
 app.use(express.json());
 
 const cors = require('cors');
@@ -21,5 +21,5 @@ app.post('/accounts/create', createNewAccounts);
 app.get('/bankhistory', getBankHistory); // Register the bank history route
 
 app.listen(port, () => {
-  console.log('Server listening on port 5000');
+  console.log(`Server listening on port ${port}`);
 });
